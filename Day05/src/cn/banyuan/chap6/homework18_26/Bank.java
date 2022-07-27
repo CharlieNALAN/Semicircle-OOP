ce8d32596aa4c6e3b9f4181485684756c61e438a
CharlieNAlan <wangjunmiao1209@163.com>
2022-07-27 15:14:30 +0800

    commit Practices

--- Day05/src/cn/banyuan/chap6/homework18_26/Bank.java
+++ Day05/src/cn/banyuan/chap6/homework18_26/Bank.java
@@ -1,7 +1,33 @@
 package cn.banyuan.chap6.homework18_26;
 
 public class Bank {
-//    public Account openAccount(long id, String password, int type){
-//
-//    }
+    public Account openAccount(long id, String password, int type){
+        if(type==0)
+            return new Account(id,0,password);
+        else if(type==1)
+            return new SavingAccount(id,0,password,6);
+        else
+            return new CreditAccount(id,0,password,100);
+    }
+
+    public double deposit(Account a,double amount){
+        a.setBalance(a.getBalance()+amount);
+        return a.getBalance();
+    }
+
+    public double withdraw(Account a,double amount){
+        if(a.getBalance()>amount){
+            a.setBalance(a.getBalance()-amount);
+            return a.getBalance();
+        }else{
+            if(a instanceof CreditAccount){
+                a.setBalance(a.getBalance()-amount);
+                return a.getBalance();
+            }
+            else {
+                System.out.println("余额不足");
+                return a.getBalance();
+            }
+        }
+    }
 }
--- Day05/src/cn/banyuan/chap6/homework18_26/Test.java
+++ Day05/src/cn/banyuan/chap6/homework18_26/Test.java
@@ -2,14 +2,21 @@ package cn.banyuan.chap6.homework18_26;
 
 public class Test {
     public static void main(String[] args) {
-        Account account = new Account(111, 111, "123456");
-        account.setPassword("111");
-        account.setPassword("111111");
-        System.out.println(account.getPassword());
+//        Account account = new Account(111, 111, "123456");
+//        account.setPassword("111");
+//        account.setPassword("111111");
+//        System.out.println(account.getPassword());
+//
+//        SavingAccount sa = new SavingAccount(111, 2222, "123456", 15);
+//        sa.setInterestRate(15);
+//        sa.setInterestRate(6);
+//        System.out.println(sa.getInterestRate());
 
-        SavingAccount sa = new SavingAccount(111, 2222, "123456", 15);
-        sa.setInterestRate(15);
-        sa.setInterestRate(6);
-        System.out.println(sa.getInterestRate());
+
+        Bank bank = new Bank();
+        Account a = bank.openAccount(111,"111111",1);
+        System.out.println(bank.deposit(a,100));
+        System.out.println(bank.withdraw(a,20));
+
     }
 }
