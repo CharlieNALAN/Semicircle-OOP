264020c2508d71c64d8b50e97a3e1c87db683f26
CharlieNAlan <wangjunmiao1209@163.com>
2022-07-28 11:02:47 +0800

    fixed

---
+++ Day05/src/test/TestMain.java
@@ -0,0 +1,28 @@
+package test;
+
+class Super{
+    public static void m1(){
+        System.out.println("m1 in Super");
+    }
+    public void m2(){
+        System.out.println("m2 in Super");
+    }
+}
+class Sub extends Super{
+    public static void m1(){
+        System.out.println("m1 in Sub");
+    }
+    public void m2(){
+        System.out.println("m2 in Sub");
+    }
+}
+public class TestMain{
+    public static void main(String args[]){
+        Super sup = new Sub();
+        sup.m1();
+        sup.m2();
+        Sub sub = (Sub) sup;
+        sub.m1();
+        sub.m2();
+    }
+}
