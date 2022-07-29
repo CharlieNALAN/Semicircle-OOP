62f916c6de24475650a8d7acfbb3525c2806a5d0
CharlieNAlan <wangjunmiao1209@163.com>
2022-07-29 11:18:37 +0800

    commit Practice and demo -> about Exception

---
+++ Day07/src/cn/banyuan/demo1/Practice1.java
@@ -0,0 +1,22 @@
+package cn.banyuan.demo1;
+
+import java.util.Scanner;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        System.out.print("请输入课程代号（1~3之间):");
+        try {
+            Scanner sc = new Scanner(System.in);
+            String[] tmp = {"aaa","bbb","ccc"};
+            int a = sc.nextInt();
+            System.out.println(tmp[a-1]);
+
+        }catch (IndexOutOfBoundsException e){
+            e.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            System.out.println("欢迎提出建议");
+        }
+    }
+}
