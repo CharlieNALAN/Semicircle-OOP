e64a24f644b4716efb0a1b059dce64e9e232e835
CharlieNAlan <wangjunmiao1209@163.com>
2022-08-01 15:08:09 +0800

    fixed

---
+++ Biz/src/main/java/MobileCard.java
@@ -0,0 +1,15 @@
+public class MobileCard {
+    public String cardNumber;
+    public String userName;
+    public String passWord;
+    public ServicePackage serPackage;
+    public double consumAmount;
+    public double money;
+    public int realTalkTime;
+    public int realSMSCount;
+    public int realFlow;
+
+    public void showMeg(){
+
+    }
+}
---
+++ Biz/src/main/java/ServicePackage.java
@@ -0,0 +1,2 @@
+public class ServicePackage {
+}
