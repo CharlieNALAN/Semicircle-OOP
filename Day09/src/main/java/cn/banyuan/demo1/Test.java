49bf9fb6fa5b03ec6d5a2aef20636d553448df27
CharlieNAlan <wangjunmiao1209@163.com>
2022-08-02 10:32:00 +0800

    commit demo ->about Packaging class

---
+++ Day09/src/main/java/cn/banyuan/demo1/Test.java
@@ -0,0 +1,24 @@
+package cn.banyuan.demo1;
+
+public class Test {
+    public static void main(String[] args) {
+        Byte v1 = new Byte((byte) 3);
+        Short v2 = new Short((short) 3);
+        Integer v3 = new Integer(3);
+        Long v4 = new Long(4);
+        Float v5 = new Float(3.0);
+        Double v6 = new Double(3.0);
+        Character v7 = new Character('3');
+        Boolean v8 = new Boolean(true);
+
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(v3);
+        System.out.println(v4);
+        System.out.println(v5);
+        System.out.println(v6);
+        System.out.println(v7);
+        System.out.println(v8);
+
+    }
+}
